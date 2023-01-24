de7d7b1dfdea0c74f47408c3185f1513573d2f89
Josep Llaneras <josep.llaneras@proton.ch>
2023-01-24 18:44:39 +0100

    Fix pylint warnings [VPNLINUX-438]

--- proton/keyring_linux/secretservice/__init__.py
+++ proton/keyring_linux/secretservice/__init__.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-module-docstring
 from .secretservice_backend import KeyringBackendLinuxSecretService
 
 __all__ = ["KeyringBackendLinuxSecretService"]
--- proton/keyring_linux/secretservice/secretservice_backend.py
+++ proton/keyring_linux/secretservice/secretservice_backend.py
@@ -1,10 +1,14 @@
+"""Secret Service Keyring backend."""
+
 import logging
 from proton.keyring_linux.core import KeyringBackendLinux
 
 logger = logging.getLogger(__name__)
 
 
+# pylint: disable=too-few-public-methods
 class KeyringBackendLinuxSecretService(KeyringBackendLinux):
+    """Implements the Secret Service keyring backend."""
     @classmethod
     def _get_priority(cls):
         return 5.
@@ -12,6 +16,7 @@ class KeyringBackendLinuxSecretService(KeyringBackendLinux):
     @classmethod
     def _validate(cls):
         try:
+            # pylint: disable=import-outside-toplevel
             from keyring.backends import SecretService
             return cls._is_backend_working(SecretService.Keyring())
         except ModuleNotFoundError:
@@ -19,5 +24,6 @@ class KeyringBackendLinuxSecretService(KeyringBackendLinux):
             return False
 
     def __init__(self):
+        # pylint: disable=import-outside-toplevel
         from keyring.backends import SecretService
         super().__init__(SecretService.Keyring())
